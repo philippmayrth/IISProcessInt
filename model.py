37537334e0d9e0ff472dc0a9b24440fc48c8b455
Philipp Mayr <philipp.mayr@hs-augsburg.de>
2023-10-30 15:24:14 +0100

    first clean up iteration over model script

--- model.py
+++ model.py
@@ -1,54 +1,33 @@
-# %%
-from sklearn.tree import DecisionTreeClassifier
+from typing import *
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import recall_score, precision_score, accuracy_score
 import pandas as pd
 import sqlite3
 from pathlib import Path
+import pm4py
+from pm4py.objects.log.importer.xes import importer as xes_importer
+from pm4py.algo.discovery.alpha import algorithm as alpha_miner
 
 
-# %%
-dbPath = Path("../data/data.sqlite")
-if not dbPath.exists():
-    # Avoid creating an empty database
-    raise Exception("Database file does not exist")
-con = sqlite3.connect(dbPath)
+# Enhance dataframe with start and end activities
+def getActivityByOrderingTimestamp(caseId, order: str) -> Optional[str]:
+    cursor = con.cursor()
+    cursor.execute(f"select ACTIVITY_EN from Pizza_Event where _case_key = ? order by eventtime {order} limit 1", (caseId, ))
+    res = cursor.fetchone()
+    cursor.close()
+    if len(res) == 0:
+        return None
+    return res[0]
 
-# %%
-customerTypeMap = ["Student", "Teenager", "Adult", "Senior"]
-customerLocationMap = ["Munich District One","Munich District Three","Munich District Two","Munich District Four","Munich District Five"]
-distributionChannelMap =  ["Feedera SE", "Deliver Now Holding", "Deliveruu Inc.", "Orderly SE", "BestOrder Inc.", "TownExpress Inc.", "Heropizza Lmtd."]
-weekdayMap = ["Sunday", "Tuesday", "Friday", "Saturday", "Wednesday", "Monday", "Thursday"]
-costfactorMap = ["","Chef 2","Chef 1","Ingredients","Delivery Scooters","Phone Bill","Delivery Guy 2","Distribution channel fees","Waiter","Delivery Guy 1"]
-sizeMap = ["Medium", "Small", "Large"]
-typeMap = ["Funghi", "Salami", "Calzone", "Speciale", "Magherita", "Paprika", "Veggie"]
 
-# %%
-df = pd.read_sql("SELECT * FROM Pizza_Case WHERE Variant != 5", con)
-# df.drop(["_CASE_KEY"], axis=1, inplace=True)
-df.drop("Customer_ID", axis=1, inplace=True)
+def getStartActivity(caseId) -> Optional[str]:
+    return getActivityByOrderingTimestamp(caseId, "asc")
 
-# df["CustomerType"] = df["CustomerType"].map(customerTypeMap.index)
-# df["CustomerLocation"] = df["CustomerLocation"].map(customerLocationMap.index)
-# df["DistributionChannel"] = df["DistributionChannel"].map(distributionChannelMap.index)
-# df["Weekday"] = df["Weekday"].map(weekdayMap.index)
-# df["CostFactor"] = df["CostFactor"].map(costfactorMap.index)
-# df["PizzaSize"] = df["PizzaSize"].map(sizeMap.index)
-# df["PizzaType"] = df["PizzaType"].map(typeMap.index)
 
-df = pd.get_dummies(df, columns=["CustomerType", "CustomerLocation", "DistributionChannel", "Weekday", "CostFactor", "PizzaSize", "PizzaType"])
-df = pd.get_dummies(df, columns=["Variant"])
+def getEndActivity(caseId) -> Optional[str]:
+    return getActivityByOrderingTimestamp(caseId, "desc")
 
-df["Profit"] = df["Revenue"] - df["Costs"]
-df["IsOrderProfitable"] = df["Profit"] > 0
-
-df
-
-# %%
-# Extract the features and target variable
-# X = df.drop(['CustomerSatisfaction'], axis=1)
-# y = df['CustomerSatisfaction']
 
 def algo(df, column: str):
     df = df.copy()
@@ -83,101 +62,75 @@ def algo(df, column: str):
     for feature, importance in zip(X.columns, importances):
         print(feature, importance)
 
-# %%
-algo(df, "CustomerSatisfaction")
-
-# %%
-algo(df, "Profit")
-
-# %%
-# Drop Revenue and Costs because that would train the model to give us a non useful result
-dfPredictPrfitablity = df.copy()
-dfPredictPrfitablity.drop("Profit", axis=1, inplace=True)
-dfPredictPrfitablity.drop("Costs", axis=1, inplace=True)
-dfPredictPrfitablity.drop("Revenue", axis=1, inplace=True)
-algo(dfPredictPrfitablity, "IsOrderProfitable")
-
-# %%
-dfPredictCustomerSatisfaction = df.copy()
-dfPredictCustomerSatisfaction["IsCustomerSatisfied"] = dfPredictCustomerSatisfaction["CustomerSatisfaction"] >= 3
-algo(dfPredictCustomerSatisfaction, "CustomerSatisfaction")
-
-# %%
-import pm4py
-from pm4py.objects.log.importer.xes import importer as xes_importer
-from pm4py.algo.discovery.alpha import algorithm as alpha_miner
 
 
+def prepareData(df: pd.DataFrame) -> pd.DataFrame:
+    df.drop("Customer_ID", axis=1, inplace=True)
 
-# %%
-log = xes_importer.apply("../data/Pizza_Event.xes")
+    df = pd.get_dummies(df, columns=["CustomerType", "CustomerLocation", "DistributionChannel", "Weekday", "CostFactor", "PizzaSize", "PizzaType"])
+    df = pd.get_dummies(df, columns=["Variant"])
 
-# %%
-log
+    df["Profit"] = df["Revenue"] - df["Costs"]
+    df["IsOrderProfitable"] = df["Profit"] > 0
 
-# %%
-net, initial_marking, final_marking = alpha_miner.apply(log)
+    # Drop Revenue and Costs because that would train the model to give us a non useful result
+    dfPredictPrfitablity = df.copy()
+    dfPredictPrfitablity.drop("Profit", axis=1, inplace=True)
+    dfPredictPrfitablity.drop("Costs", axis=1, inplace=True)
+    dfPredictPrfitablity.drop("Revenue", axis=1, inplace=True)
+    algo(dfPredictPrfitablity, "IsOrderProfitable")
 
-# %%
-process_model = pm4py.discover_bpmn_inductive(log)
-pm4py.view_bpmn(process_model)
+    dfPredictCustomerSatisfaction = df.copy()
+    dfPredictCustomerSatisfaction["IsCustomerSatisfied"] = dfPredictCustomerSatisfaction["CustomerSatisfaction"] >= 3
+    algo(dfPredictCustomerSatisfaction, "CustomerSatisfaction")
 
-# %%
-pm4py.stats.get_case_duration(log, case_id="895")
+    log = xes_importer.apply("../data/Pizza_Event.xes")
+    net, initial_marking, final_marking = alpha_miner.apply(log)
 
-# %%
-df
+    process_model = pm4py.discover_bpmn_inductive(log)
+    pm4py.view_bpmn(process_model)
 
-# %%
-from typing import *
-# Enhance dataframe with start and end activities
-def getActivityByOrderingTimestamp(caseId, order: str) -> Optional[str]:
-    cursor = con.cursor()
-    cursor.execute(f"select ACTIVITY_EN from Pizza_Event where _case_key = ? order by eventtime {order} limit 1", (caseId, ))
-    res = cursor.fetchone()
-    cursor.close()
-    if len(res) == 0:
-        return None
-    return res[0]
+    pm4py.stats.get_case_duration(log, case_id="895")
 
-def getStartActivity(caseId) -> Optional[str]:
-    return getActivityByOrderingTimestamp(caseId, "asc")
+    df["Duration"] = df["_CASE_KEY"].map(lambda x: pm4py.stats.get_case_duration(log, case_id=str(x)))
+    df["StartActivity"] = df["_CASE_KEY"].map(getStartActivity)
+    df["EndActivity"] = df["_CASE_KEY"].map(getEndActivity)
+    df.drop("_CASE_KEY", axis=1, inplace=True)
 
-def getEndActivity(caseId) -> Optional[str]:
-    return getActivityByOrderingTimestamp(caseId, "desc")
-
-
-# %%
-# Drop all cases of variant 5 because they are not in the log
-#df = df[df["Variant"] != 5]
+    # convert startactity with one hot encoding
+    df = pd.get_dummies(df, columns=["StartActivity"])
+    df = pd.get_dummies(df, columns=["EndActivity"])
+    return df
 
 
-df["Duration"] = df["_CASE_KEY"].map(lambda x: pm4py.stats.get_case_duration(log, case_id=str(x)))
-df["StartActivity"] = df["_CASE_KEY"].map(getStartActivity)
-df["EndActivity"] = df["_CASE_KEY"].map(getEndActivity)
-df.drop("_CASE_KEY", axis=1, inplace=True)
-df
+if __name__ == "__main__":
+    dbPath = Path("../data/data.sqlite")
+    if not dbPath.exists():
+        # Avoid creating an empty database
+        raise Exception("Database file does not exist")
+    con = sqlite3.connect(dbPath)
+    
 
-# %%
-# convert startactity with one hot encoding
-df = pd.get_dummies(df, columns=["StartActivity"])
-df = pd.get_dummies(df, columns=["EndActivity"])
+    df = pd.read_sql("SELECT * FROM Pizza_Case WHERE Variant != 5", con)
+    df = prepareData(df)
+    # Drop Revenue and Costs because that would train the model to give us a non useful result
+    dfPredictPrfitablity = df.copy()
 
-# %%
-# Drop Revenue and Costs because that would train the model to give us a non useful result
-dfPredictPrfitablity = df.copy()
-
-dfPredictPrfitablity.drop("Profit", axis=1, inplace=True)
-dfPredictPrfitablity.drop("Costs", axis=1, inplace=True)
-dfPredictPrfitablity.drop("Revenue", axis=1, inplace=True)
-algo(dfPredictPrfitablity, "IsOrderProfitable")
-
-# %%
-dfPredictCustomerSatisfaction = df.copy()
-dfPredictCustomerSatisfaction["IsCustomerSatisfied"] = dfPredictCustomerSatisfaction["CustomerSatisfaction"] >= 3
-algo(dfPredictCustomerSatisfaction, "CustomerSatisfaction")
-
-# %%
+    dfPredictPrfitablity.drop("Profit", axis=1, inplace=True)
+    dfPredictPrfitablity.drop("Costs", axis=1, inplace=True)
+    dfPredictPrfitablity.drop("Revenue", axis=1, inplace=True)
+    algo(dfPredictPrfitablity, "IsOrderProfitable")
 
 
+    dfPredictCustomerSatisfaction = df.copy()
+    dfPredictCustomerSatisfaction["IsCustomerSatisfied"] = dfPredictCustomerSatisfaction["CustomerSatisfaction"] >= 3
+    algo(dfPredictCustomerSatisfaction, "CustomerSatisfaction")
+
+    
+
+
+
+
+
+    
 
